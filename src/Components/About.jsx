c559809d849822fce3b6cd3095437b12ca5e2480
Nate <natemausert@gmail.com>
2022-01-05 21:56:56 -0500

    added massive customization, need to teak overflow settings and max text-height

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,53 +6,41 @@ import '../Styles/About.css'
 
 const About = (props) => {
 
-  function showICE () {
-    
-  }
-
   function animate () {
-    const commands = String.raw`Krang@internal-Tessier-Ashpool/:~ cd 
-Krang@Dixie-Flatline/Wintermute/:~ ls`;
+    const commands = String.raw`NathanMausert@internal-Tessier-Ashpool/:~ cd 
+    ../Ono-Sendai/Dixie-Flatline/FREESIDE-SERVER.js :~ ls`;
 
 const beep = String.raw`
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+        ______   ______     ______     ______     __     ______     ______        
+        /\__  _\ /\  ___\   /\  ___\   /\  ___\   /\ \   /\  ___\   /\  == \       
+        \/_/\ \/ \ \  __\   \ \___  \  \ \___  \  \ \ \  \ \  __\   \ \  __<       
+          \ \_\  \ \_____\  \/\_____\  \/\_____\  \ \_\  \ \_____\  \ \_\ \_\     
+            \/_/   \/_____/   \/_____/   \/_____/   \/_/   \/_____/   \/_/ /_/     
+                                                                                  
+        ______     ______     __  __     ______   ______     ______     __        
+        /\  __ \   /\  ___\   /\ \_\ \   /\  == \ /\  __ \   /\  __ \   /\ \       
+        \ \  __ \  \ \___  \  \ \  __ \  \ \  _-/ \ \ \/\ \  \ \ \/\ \  \ \ \____  
+        \ \_\ \_\  \/\_____\  \ \_\ \_\  \ \_\    \ \_____\  \ \_____\  \ \_____\ 
+          \/_/\/_/   \/_____/   \/_/\/_/   \/_/     \/_____/   \/_____/   \/_____/ 
+                                                                                  
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #`;
 
-          _____                    _____                    _____                    _____          
-         /\    \                  /\    \                  /\    \                  /\    \         
-        /::\    \                /::\    \                /::\    \                /::\    \        
-       /::::\    \              /::::\    \              /::::\    \              /::::\    \       
-      /::::::\    \            /::::::\    \            /::::::\    \            /::::::\    \      
-     /:::/\:::\    \          /:::/\:::\    \          /:::/\:::\    \          /:::/\:::\    \     
-    /:::/__\:::\    \        /:::/__\:::\    \        /:::/__\:::\    \        /:::/__\:::\    \    
-   /::::\   \:::\    \      /::::\   \:::\    \      /::::\   \:::\    \      /::::\   \:::\    \   
-  /::::::\   \:::\    \    /::::::\   \:::\    \    /::::::\   \:::\    \    /::::::\   \:::\    \  
- /:::/\:::\   \:::\ ___\  /:::/\:::\   \:::\    \  /:::/\:::\   \:::\    \  /:::/\:::\   \:::\____\ 
-/:::/__\:::\   \:::|    |/:::/__\:::\   \:::\____\/:::/__\:::\   \:::\____\/:::/  \:::\   \:::|    |
-\:::\   \:::\  /:::|____|\:::\   \:::\   \::/    /\:::\   \:::\   \::/    /\::/    \:::\  /:::|____|
- \:::\   \:::\/:::/    /  \:::\   \:::\   \/____/  \:::\   \:::\   \/____/  \/_____/\:::\/:::/    / 
-  \:::\   \::::::/    /    \:::\   \:::\    \       \:::\   \:::\    \               \::::::/    /  
-   \:::\   \::::/    /      \:::\   \:::\____\       \:::\   \:::\____\               \::::/    /   
-    \:::\  /:::/    /        \:::\   \::/    /        \:::\   \::/    /                \::/____/    
-     \:::\/:::/    /          \:::\   \/____/          \:::\   \/____/                  ~~          
-      \::::::/    /            \:::\    \               \:::\    \                                  
-       \::::/    /              \:::\____\               \:::\____\                                 
-        \::/____/                \::/    /                \::/    /                                 
-         ~~                       \/____/                  \/____/                                  
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #`;
-
-const Krang = String.raw`
-
-             ..    ,,,,,,    ,,,,,,      ,,,,, .,,,,     ,,,,, .,,,,,,,,,,,,,    .,,,,,,    ,,,,,  
- #############/   #######.   #######    ###### /####,    (#### /##############   #######.   #####  
- #####((((((((/   ########   ########  ####### /####,    (#### /####*   /#####  *########   #####  
- #####((((*      #########.  ################# /####,    (#### /####*   ######  #########.  #####  
- ############.   #### (####  ################# /####,    (#### /############## *#### #####  (*,..  
- .############  #####  ####. ################# /####,    (##(( *,.    .,*/((##  ####/ #####  ####( 
-   /##########*,####,  (###( (,.    .,*/( ##### #####     ####( (###########   #####  *####  ####( 
-            (### ####   (#### ##### .###* ##### ######(/######, (####. #####(  ####/   ##### ####( 
-            /###.###,    ####.#####  ###  ####( .#############  (####.  #####.(####    /#### ####(
+const Kuang = String.raw`
+About Me:
+I'm New York and Los Angeles based. When it comes to my work, 
+I have an interest in projects that improve users lives.
+I aim to solve problems on both interpersonal and logistical levels.
+I'm driven by a passion for providing tangible and meaningful results I can be proud of.
+My aim is to offer well thought out and expertly crafted solutions to complex problems with intention and strong communication skills.
+My background in medical welding and project management of prototype cryogenic pressure vessels gives me solid troubleshooting fundamentals 
+and an aptitude for managing expansive projects with unexpected issues.
+My education at General Assembly has given me practical hands-on experience with
+computer science fundamentals and the industries most in demand technologies.
+Currently, I'm looking for either a role in full stack web development, or game development!
+Also I'm a fan of William Gibson's Neuromancer.
+See You Chiba City.
 `;
 
 let blink = document.querySelector('.blink');
@@ -77,7 +65,7 @@ const RenderString = characters => {
 
 const TypeString = async characters => {
 	for(const character of characters.split('')) {
-		await Delay(RandomNumber(100, 200));
+		await Delay(RandomNumber(50, 50));
 		RenderString(character);
 	}
 }
@@ -104,20 +92,27 @@ const DrawCommands = async commands => {
 
 // Start the code
 (async()=> {
-	await DrawCommands("/:~ ssh coffeegremlin@internal-Tessier-Ashpool -p 2000");
-	await Delay(7000);
-	RenderString("coffeegremlin@internal-Tessier-Ashpool password: ");
-	await Delay(2000);
-	RenderString("npm run Dixie-Flatline.js");
+	await DrawCommands("/:~ ssh NathanMausert@internal-Tessier-Ashpool -p 2000");
+	await Delay(1000);
+	RenderString("NathanMausert@internal-Tessier-Ashpool password:");
+	await Delay(3000);
+  RenderString("\n")
+	RenderString("npm run Kuang.js ");
 	await DrawCommands(commands);
-	RenderString('\nKrang.js    BOCKRIS-SERVER.js\n\n');
-	await DrawCommands('coffeegremlin@internal-Tessier-Ashpool/ICE:~ node KRANG.js');
+	RenderString('\nbeep.js   FREESIDE-SERVER.js\n\n    BOCKRIS-SERVER.js\n\n');
+	await DrawCommands('NathanMausert@internal-Tessier-Ashpool/ICE:~ node KUANG.js');
+  await DrawCommands(commands);
 	await DrawLines( beep );
-	await TypeString("\n\n ROM --McCoy Pauley-- LOG: Case, you better keep your promise after this. Sense/Net existence is no fun.");
+	await TypeString("\n\ncd ./Hosaka-Storage/Ono-Sendai/ROM/McCoy-Pauley.py \nLOG: Case, you better keep your promise after this");
 	await Delay(2000);
-	ResetTerminal();
-	await DrawCommands('coffeegremlin@internal-Tessier-Ashpool:~ KEY=3db7ca618243da1ba3bc76ab14bcf07b node KRANG.js');
-	await DrawLines(Krang);
+  ResetTerminal();
+	await DrawLines("\nNathanMausert@internal-Tessier-Ashpool:~ KEY=3db7ca618243da1ba3bc76ab14bcf07b node KUANG.js");
+  await DrawLines("\nCONNECTED")
+  await DrawLines("\n(freeside-server) -> git:(main) ls")
+  await DrawLines("\n(freeside-server) -> git:(main) ABOUTME.txt  package-lock.json  package.json  winter_mute")
+  await DrawLines("\n(freeside-server) -> git:(main) code .")
+	await DrawLines(Kuang);
+  await DrawLines("\n(freeside-server) -> git:(main) ")
   
 })();
 
@@ -126,13 +121,13 @@ const DrawCommands = async commands => {
   const Terminal = () => {
     return(
       <div>
-        <p>Krang MK. IV I.C.E. Breaker loaded.</p>
-        <p>Target: NATHAN MAUSERT PORTFOLIO Node.js</p>
-        <p>FILE: '../../AboutMe.txt'</p>
+        <p>Kuang MK IV I.C.E. Breaker loaded.</p>
+        <p>target.FREESIDE-SERVER.js /:~ NATHAN-MAUSERT-PORTFOLIO Node.js</p>
+        <p>FILE: '../../Information/AboutMe.txt'</p>
         <h4>Ready Case?</h4>
       <button className='hack' onClick={animate}>Let's ride console cowboy</button>
       <div class="scanline"></div>
-      <pre class="code"><span class="blink">█</span></pre>
+      <pre class=""><span class="blink">█</span></pre>
     </div>
     )
   }
@@ -143,13 +138,12 @@ const DrawCommands = async commands => {
   return (
     <div>
       <h1>BOCKRIS SYSTEMS GmbH</h1>
-      <p>Link established to ONO-SENDAI Cyberspace 7</p>
-      <p>Load Krang MK. IV I.C.E. Breaker?</p>
+      <p>Link established to Hosaka ONO-SENDAI Cyberspace 7</p>
+      <p>Load Kuang MK IV I.C.E. Breaker?</p>
       <button className='hack' type='submit' onClick={onClick}>YES</button>
       {showTerminal ? <Terminal/> : null}
     </div>
   )
-
   
 }
 
